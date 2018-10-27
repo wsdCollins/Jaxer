b68f3b081c92d74a33a198d85763365f4aa61ff3
Benjamin Collins <collins@wsd.co.jp>
2018-10-27 04:43:53 +0100

    removing tools folder

--- tools/com.aptana.autobuild/libs/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-scriptFile=../../../products/server/framework/scripts/auto_builder.js
-buildFile=../../../products/server/framework/build.json
-
-echo Building $buildFile
-java -jar auto-builder.jar $scriptFile $buildFile
